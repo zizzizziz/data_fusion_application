2689bb96336036fec9c0b69f440361a987224fb7
Hmmmx <862020701@qq.com>
2023-07-24 17:43:08 +0800

    Staff complete

---
+++ code/quick-started/target/classes/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tbl_user_info (userName, userId, updateTime) VALUES('ldncs', 666, 202304041504);
+INSERT INTO tbl_user_info (userName, userId, updateTime) VALUES('ldncs', 777, 202304041520);
+INSERT INTO tbl_user_info (userName, userId, updateTime) VALUES('ldncs', 888, 202304041578);
---
+++ code/quick-started/target/classes/db/table.sql
@@ -0,0 +1,7 @@
+
+create table if not exists tbl_user_info(
+    userName varchar(50),
+    userId int,
+    updateTime bigint
+    primary key auto_increment
+);
